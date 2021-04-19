194e96809ba3df5eeea510424fd4889d5f14d235
lucianosergiodasilva <lucianomirabolant@gmail.com>
2021-04-19 16:13:15 -0300

    Primeiro commit

---
+++ Enum/Genero.cs
@@ -0,0 +1,14 @@
+namespace DIO.Series
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Documentario = 4,
+        Drama = 5,
+        Fantaria = 6,
+        Musical = 7,
+        Suspense = 8
+    }
+}
